30be11e5fd1d02f47a42d22ee9d8ba5057451832
Maxence Fuzellier <maxencefuzellier@gmail.com>
2020-02-11 15:42:49 +0100

    creation tibble top 250 via api

--- anime.R
+++ anime.R
@@ -1,21 +1,22 @@
-install.packages("jsonlite")
-library("jsonlite")
-install.packages("tidyverse")
-library(tidyverse)
+install.packages('jsonlite')
+install.packages('tidyverse')
 
-individus<-250
-individus<-250%/%50
+library('jsonlite')
+library('tidyverse')
 
-for (i in 1:individus){
+individus <- 250 %/% 50
+
+for (i in 1:individus) {
 	url_req <- paste("https://api.jikan.moe/v3/top/anime/", i, sep="")
 	list_json1 <- fromJSON(url_req)
 
-	if (missing(tibble1) == TRUE){
-		tibble1 <- list_json1$top
-	}else{
-		inter_json <- 
+	if (exists('tibble1') == FALSE){
+	  tibble1 <- tibble()
+	  tibble1 <- as_tibble(rbind(tibble1, as_tibble(list_json1$top)))
+	} else {
+	  tibble1 <- as_tibble(rbind(tibble1, as_tibble(list_json1$top)))
 	}
 }
 
-df_json1 <- as_tibble(inter_json, validate = F) 
-print(df_json1)
+#df_json1 <- as_tibble(inter_json, validate = F) 
+#print(df_json1)
